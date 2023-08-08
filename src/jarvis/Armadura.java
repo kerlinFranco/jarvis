b4f3a971ad707cb1afc67ee73f0df826b6879944
kerlinFranco <kerlinfranco@gmail.com>
2023-08-08 19:34:07 -0300

    Update Armadura.java

--- src/jarvis/Armadura.java
+++ src/jarvis/Armadura.java
@@ -5,15 +5,30 @@
  */
 package jarvis;
 
+import java.util.Scanner;
+
 /**
  *
  * @author USURIO
  */
 public class Armadura {
+    static Scanner leer=new Scanner(System.in);
     private int salud=100;
     private Bota[] botas=new Bota[2];
     private Guante[] guantes=new Guante[2];
     private double Bateria=100;
     
-    
+    public void caminar(){
+        System.out.println("Cuantos pasos desea caminar");
+        int pasos=leer.nextInt();
+       if(Bateria>=pasos){
+          for (int i=0; i<= pasos; i++) {
+            Bateria--;
+        } 
+       }else{
+           System.out.println("Bateria insuficiente");
+       }
+        
+        
+    }
 }
